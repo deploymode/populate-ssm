e158ec13c92567a698ef4d04d5888c0804581af3
Joe Niland <joe@originalmind.com.au>
2021-04-13 15:21:58 +1000

    Add requires.txt to setup.py

--- setup.py
+++ setup.py
@@ -23,4 +23,5 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+    install_requires=[">=1.14.1", "python-dotenv>=0.13.0"],
 )
